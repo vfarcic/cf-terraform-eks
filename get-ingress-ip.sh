6e7033ff08eeb13845bd4d7a3b61170085888400
Viktor Farcic <viktor@farcic.com>
2020-10-13 01:20:39 +0200

    Let there be light

---
+++ get-ingress-ip.sh
@@ -0,0 +1,12 @@
+while [ "$INGRESS_HOST" = "" ]; do
+    export INGRESS_HOSTNAME=$(kubectl \
+        --namespace ingress-nginx \
+        get svc ingress-nginx-controller \
+        --output jsonpath="{.status.loadBalancer.ingress[0].hostname}")
+    export INGRESS_HOST=$(\
+        dig +short $INGRESS_HOSTNAME \
+        | tail -1)
+    sleep 1
+done
+
+echo $INGRESS_HOST
